f435b36e9cec2fcc1ae1aa936e698c86260635cc
Nermeen Ibrahim <Nermeen.Ibrahim@linkdev.com>
2018-02-25 15:24:16 +0200

    Adding photo gallery in html, css, js

--- js/controll.js
+++ js/controll.js
@@ -1,1 +1,9 @@
-
+$(document).ready(function () {
+     $(".gallery").owlCarousel({
+        items: 4,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:5000,
+        autoplayHoverPause: true,
+    });
+});
