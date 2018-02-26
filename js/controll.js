eb5600a3dba91674660851b3f40b64d2c7c6d283
reham <rehamweb@gmail.com>
2018-02-26 13:54:52 +0200

    edit for section event

--- js/controll.js
+++ js/controll.js
@@ -11,3 +11,16 @@ $(function () {
         dots:false
     });
 });
+$(function () {
+    $("#carts").owlCarousel({
+        items:1,
+        loop: true,
+        autoplay:true,
+        autoplayTimeout:3000,
+        autoplayHoverPause: true,
+        nav:true,
+        navText: ["<span class='fas fa-angle-left'></span>","<span class='fas fa-angle-right'></span>"],
+        dots:false
+    });
+});
+
